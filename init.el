7abb69650a8adb8cb528de1f6ba8c09fdd06d293
Mårten Larsson <mala235813@gmail.com>
2020-12-17 14:34:33 +0100

    Use Fira Mono font

--- init.el
+++ init.el
@@ -3,3 +3,4 @@
 (scroll-bar-mode -1)    ; Disable visible scrollbar
 (tool-bar-mode -1)      ; Disable the toolbar
 (set-fringe-mode 10)    ; Set a left and right margin
+(set-face-attribute 'default nil :font "Fira Mono:antialias=subpixel" :height 200)
